cfd4caeb50b09c0c129c46edae895304e9cc7e8a
Mohamed Mansour <hello@mohamedmansour.com>
2014-01-18 08:27:31 -0800

    Forgot missing file for OTR Override

---
+++ Backend/Data/OTRMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using XMPP;
+using XMPP.tags.jabber.client;
+using Tags = XMPP.tags;
+
+namespace Backend.Data
+{
+    public class OTRMessage : Tags.jabber.client.message
+    {
+        public OTRMessage(Tags.jabber.client.message message) : base(message)
+        {
+            this.UpdatedBodyElements = new List<string>();
+        }
+
+        public IEnumerable<string> UpdatedBodyElements
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<string> MessageBodyElements
+        {
+            get
+            {
+                if (this.UpdatedBodyElements.Count() > 0)
+                {
+                    return from n in this.UpdatedBodyElements select n;
+                }
+                else
+                {
+                    return from n in this.bodyElements select n.Value;
+                }
+            }
+        }
+    }
+}
